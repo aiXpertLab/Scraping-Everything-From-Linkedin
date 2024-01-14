b27eefc2cec133f8d1ac2db3091a40d13bddfbf8
aiXpert <45121432+aiXpertLab@users.noreply.github.com>
2024-01-14 13:00:34 -0500

    v1.1 user

--- src/Linkedin/my_modules.py
+++ src/Linkedin/my_modules.py
@@ -1,2 +1,27 @@
-job_titles ,company_names ,company_locations ,work_methods ,post_dates ,work_times , job_desc = [], [], [], [], [], [], []
+'''
+My Module: including:
+1. Linkedin_scraping_all_jobs_details 
+'''
+
+# Linkedin_scraping_all_jobs_details
+job_titles = []
+company_names = []
+company_locations = []
+work_methods = []
+post_dates = []
+work_times = []
+job_desc = []
+
 i, j = 0, 0
+
+def add_job_data(title, company, location, method, date, time, desc):
+    """
+    Function to add job data to the lists
+    """
+    job_titles.append(title)
+    company_names.append(company)
+    company_locations.append(location)
+    work_methods.append(method)
+    post_dates.append(date)
+    work_times.append(time)
+    job_desc.append(desc)
